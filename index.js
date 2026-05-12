2a0c94f92d9b5c59708b3e0aaa8fac23bf688e0c
jaaimesanchez <jsanchezramirez235@gmail.com>
2026-05-12 22:21:57 +0200

    configuracion del servidor y puerto

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello, World!');
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const PORT = 5878;
+
+app.listen(PORT, () => {
+    console.log(`Servidor funcionando en puerto ${PORT}`);
+});
