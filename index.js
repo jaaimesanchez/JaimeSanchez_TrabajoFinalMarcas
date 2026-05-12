6195a9bb82df63a8506918592a2913457a1b1261
jaaimesanchez <jsanchezramirez235@gmail.com>
2026-05-12 22:52:47 +0200

    coleccion de objetos principal

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require("express"); 
 const app = express();
 
 app.use(express.json());
@@ -8,3 +8,38 @@ const PORT = 5878;
 app.listen(PORT, () => {
     console.log(`Servidor funcionando en puerto ${PORT}`);
 });
+
+let videojuegos = [
+    {
+        id: 1,
+        nombre: "FIFA 25",
+        genero: "Deportes",
+        plataforma: "PS5",
+        precio: 69.99,
+        desarrolladora: "EA Sports",
+        fechaLanzamiento: "2025-09-20",
+        stock: 50
+    },
+    {
+        id: 2,
+        nombre: "Minecraft",
+        genero: "Sandbox",
+        plataforma: "PC",
+        precio: 29.99,
+        desarrolladora: "Mojang",
+        fechaLanzamiento: "2011-11-18",
+        stock: 100
+    },
+    {
+        id: 3,
+        nombre: "Call of Duty",
+        genero: "Shooter",
+        plataforma: "Xbox",
+        precio: 79.99,
+        desarrolladora: "Activision",
+        fechaLanzamiento: "2024-10-15",
+        stock: 30
+    }
+];
+
+
